b411d379f3a634f634b81a3ad928d2f2f4500a68
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 19:00:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :material, :condition
+  attr_reader :brand, :size
   
   BRANDS = []
 
@@ -8,6 +8,11 @@ class Shoe
     @brand = brand
     BRANDS << brand
   end
+
+  def size=(size)
+    @size = size 
+    BRANDS.size = 3
+  end 
 
   def cobble
     self.condition = "new"
