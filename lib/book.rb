110c7be347da6a79c13120f846f306f10eafe2a6
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 18:50:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count,
-  attr_reader :title
+  attr_reader :title, :genre 
   
   GENRES = []
 
@@ -10,10 +10,6 @@ class Book
 
 def genre=(genre)
   @genre = genre
-end 
-
-def genre
-  @genre 
 end 
 
   def turn_page
